4e7de629846d3852cb2d49443676348b050c4bac
Kuck Swee Nien <Swee.Nien.Kuck@my.ey.com>
2024-04-02 20:22:23 +0800

    Day24

--- Day21/Day21.py
+++ Day21/Day21.py
@@ -35,14 +35,14 @@ while game_is_on:
 
     #dETECT COLLISION WITH WALL
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        game_is_on = False
-        scoreboard.game_over()
+        scoreboard.reset()
+        snake.reset()
 
     #Detect tail collission
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()
+            scoreboard.reset()
+            snake.reset()
 
 
 
