581fd7d7c6a7af11eb90ff75312dc1b608bf16c0
KuckSN <kucknien@gmail.com>
2024-04-02 21:40:47 +0800

    Day26

---
+++ Day26/LoopPanda.py
@@ -0,0 +1,11 @@
+student_dict = {
+    "student": ["Angela", "James", "Lily"],
+    "score": [56, 76, 98]
+}
+
+import pandas
+
+student_data_frame = pandas.DataFrame(student_dict)
+
+for (index, row) in student_data_frame.iterrows():
+    print(row.student)
