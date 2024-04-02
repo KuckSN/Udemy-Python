d8a12d736b2111c860a13a72c479875a55dda81b
Kuck Swee Nien <Swee.Nien.Kuck@my.ey.com>
2024-04-02 20:31:47 +0800

    Day24

--- Day21/scoreboard.py
+++ Day21/scoreboard.py
@@ -8,7 +8,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.highscore = 0 
+        with open(r"C:\Users\SN798EZ\Downloads\Self Study\Udemy-Python\Day21\data.txt") as data:
+            self.highscore=int(data.read())
         self.color("white")
         self.penup()
         self.goto(0, 270)
@@ -22,6 +23,8 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.highscore:
             self.highscore = self.score
+            with open(r"C:\Users\SN798EZ\Downloads\Self Study\Udemy-Python\Day21\data.txt", mode="w") as data:
+                data.write(f'{self.highscore}')
         self.score=0
         self.update_scoreboard()
 
