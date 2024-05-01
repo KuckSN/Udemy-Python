945ea519bb019d3ec250425c324c165928bc3f15
Kuck Swee Nien <kucknien@gmail.com>
2024-05-01 12:24:35 +0800

    Day61 WTFlask

--- Day61/main.py
+++ Day61/main.py
@@ -1,26 +1,24 @@
 from flask import Flask, render_template
-
-'''
-Red underlines? Install the required packages first: 
-Open the Terminal in PyCharm (bottom left). 
-
-On Windows type:
-python -m pip install -r requirements.txt
-
-On MacOS type:
-pip3 install -r requirements.txt
-
-This will install the packages from requirements.txt for this project.
-'''
+from flask_wtf import FlaskForm
+from wtforms import StringField
 
 
 app = Flask(__name__)
+app.secret_key = "021003241217_The_Best_Number_Combination"
+
+class LoginField(FlaskForm):
+    email = StringField("Email")
+    password = StringField("Password")
 
 
 @app.route("/")
 def home():
     return render_template('index.html')
 
+@app.route("/login")
+def login():
+    login_form = LoginField()
+    return render_template('login.html', form=login_form)
 
 if __name__ == '__main__':
     app.run(debug=True)
